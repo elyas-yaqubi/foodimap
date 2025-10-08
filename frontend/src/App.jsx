21444e6ab2d9ae33c21506e800add27472a34a5e
salonip10 <spatel42@dons.usfca.edu>
2025-10-07 21:53:55 -0700

    Update App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+
+function App() {
+    return (
+        <Router>
+            {/* Navbar stays visible on every page */}
+
+            {/* Define page routes here */}
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
