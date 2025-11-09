e0ede5188bbac5ca407223c40cf58394198ddd99
Elyas Yaqubi <elyasyaqubi13@gmail.com>
2025-11-09 12:43:55 -0800

    - Added jwt to application.properties - added "role" to LoginResponse -

--- backend/src/main/java/com/foodimap/foodimap/dto/LoginResponse.java
+++ backend/src/main/java/com/foodimap/foodimap/dto/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.foodimap.foodimap.dto;
 
+import com.foodimap.foodimap.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,11 +12,13 @@ public class LoginResponse {
     private String type = "Bearer";
     private String username;
     private String email;
+    private String role;
 
-    public LoginResponse(String token, String username, String email) {
+    public LoginResponse(String token, String username, String email, String role) {
 
         this.token = token;
         this.username = username;
         this.email = email;
+        this.role = role;
     }
 }
